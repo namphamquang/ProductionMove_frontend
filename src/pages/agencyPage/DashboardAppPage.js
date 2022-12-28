e140274b160485c1123e74f06f7c9cbab195d3f3
namphamquang <awildquangtien@gmail.com>
2022-12-29 00:49:45 +0700

    update

--- src/pages/agencyPage/DashboardAppPage.js
+++ src/pages/agencyPage/DashboardAppPage.js
@@ -1,8 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
 import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
+import { Box, Grid, Container, Typography, Select, MenuItem, InputLabel, Paper } from '@mui/material';
+import ReactApexChart from 'react-apexcharts';
+import axios from 'axios';
+import { Label } from '@mui/icons-material';
 // components
 import Iconify from '../../components/iconify';
 // sections
@@ -18,10 +23,109 @@ import {
   AppConversionRates,
 } from '../../sections/@agency/app';
 
+
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
   const theme = useTheme();
+  const [productMonth, setProductMonth] = useState([]);
+  const [productQ, setProductQ] = useState([]);
+  const getProductMonth = async () => {
+    const response = await axios.get(`http://localhost:8000/agency/statistic-year/${localStorage.getItem('id')}`);
+    setProductMonth(response.data);
+  };
+  useEffect(() => {
+    getProductMonth();
+  }, []);
+  const getProductQ = async () => {
+    const response = await axios.get(`http://localhost:8000/agency/statistic-quarter/${localStorage.getItem('id')}`);
+    setProductQ(response.data);
+  };
+  useEffect(() => {
+    getProductQ();
+  }, []);
+
+  const label = productMonth.map(p => p.month);
+  const soldMonth = productMonth.map(p => p.sold);
+  const errorMonth = productMonth.map(p => p.error);
+  const series = [{
+    name: 'Đã bán',
+    data: productQ.map(a => a.sold)
+  }];
+  const options = {
+    chart: {
+      height: 350,
+      type: 'bar',
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 10,
+        dataLabels: {
+          position: 'top', // top, center, bottom
+        },
+      }
+    },
+    dataLabels: {
+      enabled: true,
+      offsetY: -30,
+      style: {
+        fontSize: '20px',
+        colors: ["#304758"]
+      }
+    },
+
+    xaxis: {
+      categories: ["Q1", "Q2", "Q3", "Q4"],
+      position: 'top',
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false
+      },
+      crosshairs: {
+        fill: {
+          type: 'gradient',
+          gradient: {
+            colorFrom: '#D8E3F0',
+            colorTo: '#BED1E6',
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
+          }
+        }
+      },
+      tooltip: {
+        enabled: true,
+      }
+    },
+    yaxis: {
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false,
+      },
+      labels: {
+        show: false,
+        formatter: (val) => {
+          return "".concat(val).concat(" sản phẩm");
+        }
+      }
+
+    },
+    title: {
+      text: 'Thống kê sản phẩm bán ra theo quý năm 2022',
+      floating: true,
+      offsetY: 425,
+      align: 'center',
+      style: {
+        color: '#444',
+        fontSize: '18',
+        fontFamily: 'Public Sans,sans-serif',
+      }
+    }
+  }
 
   return (
     <>
@@ -35,185 +139,92 @@ export default function DashboardAppPage() {
         </Typography>
 
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'ant-design:android-filled'} />
-          </Grid>
+          <Grid item xs={12} md={6} lg={100}>
 
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="New Users" total={1352831} color="info" icon={'ant-design:apple-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
             <AppWebsiteVisits
-              title="Website Visits"
-              subheader="(+43%) than last year"
-              chartLabels={[
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
-              ]}
+              title="Thống kê sản phẩm tại đại lý năm 2022"
+              chartLabels={label}
               chartData={[
                 {
-                  name: 'Team A',
-                  type: 'column',
-                  fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                },
-                {
-                  name: 'Team B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
-                {
-                  name: 'Team C',
+                  name: 'Đã bán',
                   type: 'line',
                   fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Current Visits"
-              chartData={[
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.info.main,
-                theme.palette.warning.main,
-                theme.palette.error.main,
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates
-              title="Conversion Rates"
-              subheader="(+43%) than last year"
-              chartData={[
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject
-              title="Current Subject"
-              chartLabels={['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math']}
-              chartData={[
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ]}
-              chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppNewsUpdate
-              title="News Update"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: faker.name.jobTitle(),
-                description: faker.name.jobTitle(),
-                image: `/assets/images/covers/cover_${index + 1}.jpg`,
-                postedAt: faker.date.recent(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppOrderTimeline
-              title="Order Timeline"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: [
-                  '1983, orders, $4220',
-                  '12 Invoices have been paid',
-                  'Order #37745 from September',
-                  'New order placed #XF-2356',
-                  'New order placed #XF-2346',
-                ][index],
-                type: `order${index + 1}`,
-                time: faker.date.past(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite
-              title="Traffic by Site"
-              list={[
-                {
-                  name: 'FaceBook',
-                  value: 323234,
-                  icon: <Iconify icon={'eva:facebook-fill'} color="#1877F2" width={32} />,
+                  data: soldMonth,
                 },
                 {
-                  name: 'Google',
-                  value: 341212,
-                  icon: <Iconify icon={'eva:google-fill'} color="#DF3E30" width={32} />,
-                },
-                {
-                  name: 'Linkedin',
-                  value: 411213,
-                  icon: <Iconify icon={'eva:linkedin-fill'} color="#006097" width={32} />,
-                },
-                {
-                  name: 'Twitter',
-                  value: 443232,
-                  icon: <Iconify icon={'eva:twitter-fill'} color="#1C9CEA" width={32} />,
+                  name: 'Lâu không bán được',
+                  type: 'area',
+                  fill: 'gradient',
+                  data: errorMonth,
                 },
               ]}
             />
+
           </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
-            <AppTasks
-              title="Tasks"
-              list={[
-                { id: '1', label: 'Create FireStone Logo' },
-                { id: '2', label: 'Add SCSS and JS files if required' },
-                { id: '3', label: 'Stakeholder Meeting' },
-                { id: '4', label: 'Scoping & Estimations' },
-                { id: '5', label: 'Sprint Showcase' },
-              ]}
-            />
-          </Grid>
+          <Grid item xs={12} md={6} lg={100}>
+            <Paper>
+              <ReactApexChart options={options} series={series} type="bar" height={450} />
+          </Paper>
         </Grid>
-      </Container>
+
+
+
+        <Grid item xs={12} md={6} lg={8}>
+          <AppNewsUpdate
+            title="News Update"
+            list={[...Array(5)].map((_, index) => ({
+              id: faker.datatype.uuid(),
+              title: faker.name.jobTitle(),
+              description: faker.name.jobTitle(),
+              image: `/assets/images/covers/cover_${index + 1}.jpg`,
+              postedAt: faker.date.recent(),
+            }))}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={4}>
+          <AppTrafficBySite
+            title="Traffic by Site"
+            list={[
+              {
+                name: 'FaceBook',
+                value: 323234,
+                icon: <Iconify icon={'eva:facebook-fill'} color="#1877F2" width={32} />,
+              },
+              {
+                name: 'Google',
+                value: 341212,
+                icon: <Iconify icon={'eva:google-fill'} color="#DF3E30" width={32} />,
+              },
+              {
+                name: 'Linkedin',
+                value: 411213,
+                icon: <Iconify icon={'eva:linkedin-fill'} color="#006097" width={32} />,
+              },
+              {
+                name: 'Twitter',
+                value: 443232,
+                icon: <Iconify icon={'eva:twitter-fill'} color="#1C9CEA" width={32} />,
+              },
+            ]}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={8}>
+          <AppTasks
+            title="Tasks"
+            list={[
+              { id: '1', label: 'Create FireStone Logo' },
+              { id: '2', label: 'Add SCSS and JS files if required' },
+              { id: '3', label: 'Stakeholder Meeting' },
+              { id: '4', label: 'Scoping & Estimations' },
+              { id: '5', label: 'Sprint Showcase' },
+            ]}
+          />
+        </Grid>
+      </Grid>
+    </Container>
     </>
   );
 }
--- src/pages/guaranteePage/DashboardAppPage.js
+++ src/pages/guaranteePage/DashboardAppPage.js
@@ -1,8 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
 import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
+import { Box, Grid, Container, Typography, Select, MenuItem, InputLabel, Paper } from '@mui/material';
+import ReactApexChart from 'react-apexcharts';
+import axios from 'axios';
+import { Label } from '@mui/icons-material';
 // components
 import Iconify from '../../components/iconify';
 // sections
@@ -18,11 +23,26 @@ import {
   AppConversionRates,
 } from '../../sections/@guarantee/app';
 
+
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
   const theme = useTheme();
+  const [productMonth, setProductMonth] = useState([]);
+  const [productQ, setProductQ] = useState([]);
+  const getProductMonth = async () => {
+    const response = await axios.get(`http://localhost:8000/guarantee/statistic-year/${localStorage.getItem('id')}`);
+    setProductMonth(response.data);
+  };
+  useEffect(() => {
+    getProductMonth();
+  }, []);
 
+
+  const label = productMonth.map(p => p.month);
+  const doneMonth = productMonth.map(p => p.done);
+  const insurancingMonth = productMonth.map(p => p.insurancing);
+  const errorMonth = productMonth.map(p => p.error);
   return (
     <>
       <Helmet>
@@ -35,110 +55,33 @@ export default function DashboardAppPage() {
         </Typography>
 
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'ant-design:android-filled'} />
-          </Grid>
+          <Grid item xs={12} md={6} lg={100}>
 
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="New Users" total={1352831} color="info" icon={'ant-design:apple-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
             <AppWebsiteVisits
-              title="Thống kê sản phẩm năm 2022"
-              chartLabels={[
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
-              ]}
+              title="Thống kê sản phẩm tại đại lý năm 2022"
+              chartLabels={label}
               chartData={[
                 {
-                  name: 'Team A',
-                  type: 'column',
-                  fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                },
-                {
-                  name: 'Team B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
-                {
-                  name: 'Team C',
+                  name: 'Bảo hành xong',
                   type: 'line',
                   fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                  data: doneMonth,
+                },
+                {
+                  name: 'Đang bảo hành',
+                  type: 'area',
+                  fill: 'gradient',
+                  data: insurancingMonth,
+                },
+                {
+                  name: 'Lỗi',
+                  type: 'column',
+                  fill: 'gradient',
+                  data: errorMonth,
                 },
               ]}
             />
-          </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Current Visits"
-              chartData={[
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.info.main,
-                theme.palette.warning.main,
-                theme.palette.error.main,
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates
-              title="Conversion Rates"
-              subheader="(+43%) than last year"
-              chartData={[
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ]}
-              
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject
-              title="Current Subject"
-              chartLabels={['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math']}
-              chartData={[
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ]}
-              chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
-            />
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
@@ -150,24 +93,6 @@ export default function DashboardAppPage() {
                 description: faker.name.jobTitle(),
                 image: `/assets/images/covers/cover_${index + 1}.jpg`,
                 postedAt: faker.date.recent(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppOrderTimeline
-              title="Order Timeline"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: [
-                  '1983, orders, $4220',
-                  '12 Invoices have been paid',
-                  'Order #37745 from September',
-                  'New order placed #XF-2356',
-                  'New order placed #XF-2346',
-                ][index],
-                type: `order${index + 1}`,
-                time: faker.date.past(),
               }))}
             />
           </Grid>
